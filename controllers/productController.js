860cecb1dfd0e70266d325bca8558c6bd2724993
Jordan Blakey <454820+jordanblakey@users.noreply.github.com>
2020-11-06 17:49:34 -0600

    Complete first part of Node REST API walkthrough.

---
+++ controllers/productController.js
@@ -0,0 +1,43 @@
+const Product = require('../models/productModel')
+
+// @desc Gets All Products
+// @route GET /api/products
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll()
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(products))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// @desc Gets a single product
+// @route GET /api/product/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id)
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'Product not found' }))
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(product))
+    }
+  } catch (error) {}
+}
+
+// @desc Create a Product
+// @route POST /api/products
+async function createProduct(req, res) {
+  try {
+    const product
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {
+  getProducts,
+  getProduct
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const {
+  getProducts,
+  getProduct,
+  createProduct
+} = require('./controllers/productController')
+
+const server = http.createServer((req, res) => {
+  // Serve a basic HTML Page
+  // res.statusCode = 200
+  // res.setHeader('Content-Type', 'text/html')
+  // res.write('<h1>Hello World!</h1>')
+  // res.end()
+
+  // Serve a JSON response at an endpoint, using shorthand
+  if (req.url === '/api/products' && req.method === 'GET') {
+    getProducts(req, res)
+  } else if (
+    req.url.match(/\/api\/product\/([0-9]+)/) &&
+    req.method === 'GET'
+  ) {
+    const id = req.url.split('/')[3]
+    getProduct(req, res, id)
+  } else if (req.url === 'api/products' && req.method === 'POST') {
+    createProduct(req, res)
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Route not found.' }))
+  }
+})
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
